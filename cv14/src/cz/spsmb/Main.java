6fc33f8ac10d9e763bf22a5241770a11e309d3f5
sec <sec@spsmb.cz>
2025-03-05 12:31:16 +0100

    cv14 close reader before file delete.

--- cv14/src/cz/spsmb/Main.java
+++ cv14/src/cz/spsmb/Main.java
@@ -3,6 +3,7 @@ package cz.spsmb;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Main {
@@ -16,6 +17,7 @@ public class Main {
         }
         finally {
             System.out.println("TADY.");
+
         }
 
         Scanner scanner = new Scanner(file);
@@ -25,6 +27,19 @@ public class Main {
             data[i] = scanner.nextLine();
             i++;
         }
+        scanner.close();
         System.out.println(data[1]);
+
+        try {
+
+            Thread.sleep(1000L);
+            System.out.println(Files.deleteIfExists(file.toPath()));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
+
+
 }
